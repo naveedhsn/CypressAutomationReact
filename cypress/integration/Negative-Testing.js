07007d3eb918fcf7a5927f472d04ac673af374b2
naveedhsn <naveedhossaiin@gmail.com>
2022-05-11 17:25:19 +0600

    script update

--- cypress/integration/Negative-Testing.js
+++ cypress/integration/Negative-Testing.js
@@ -38,5 +38,93 @@ describe('QA-CODE-QUIZ', function () {
             cy.wait(3000)
             cy.visit('/')
         })
+
+        it('Email & password - #@%^%#$@#$@#.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('#@%^%#$@#$@#.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - @example.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('@example.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - Some one <email@example.com>', function(){
+            cy.get('[placeholder="Enter Username"]').type('Some one <email@example.com>')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - email.example.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('email.example.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - email@example@example.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('email@example@example.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - .email@example.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('.email@example.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - email..email@example.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('email..email@example.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Email & valid password - Abc..123@example.com', function(){
+            cy.get('[placeholder="Enter Username"]').type('Abc..123@example.com')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Random Text & password', function(){
+            cy.get('[placeholder="Enter Username"]').type('AJSsdsdasacarmvJkasjs')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Number & password', function(){
+            cy.get('[placeholder="Enter Username"]').type('135464635568461')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
+
+        it('Special character & password', function(){
+            cy.get('[placeholder="Enter Username"]').type('~!@$%^&*()_+=-')
+            cy.get('[placeholder="password"]').type('TopSecret1234!')
+            cy.get('.sc-bZQynM').click()
+            cy.wait(3000)
+            cy.visit('/')
+        })
     })
 })
